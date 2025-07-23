d76ce98b7337ba0f0e8d79c859a7454785e92bb8
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-23 18:04:16 -0400

    Modified functionality of the loop in main to handle user input and evaluate the lowest number of accidents.

--- safe_driving.cpp
+++ safe_driving.cpp
@@ -3,15 +3,46 @@
 
 std::string region_name;
 int num_accidents;
+int lowest_accidents = num_accidents;
+std::string safest_region;
 
 void getRegInfo(std::string &region, int &accidents);
 bool isLower(int first_value, int second_value);
 
 int main(){
+    int user_option;
+    std::cout << "Please enter in 1 to input the region name and number of accidents" << std::endl;
+    std::cout << "Please enter in 0 to quit and show the region with the lowest number of accidents" << std::endl;
+    std::cin >> user_option;
+    int count = 0;
+    while (user_option != 0){
 
-    getRegInfo(region_name, num_accidents);
-    std::cout << "Region name: " << region_name << std::endl;
-    std::cout << "Number of accidents: " << num_accidents << std::endl;
+        if (user_option != 1){
+            std::cout << "Invalid option, please try again." << std::endl;
+            std::cout << "Please enter in 1 to input the region name and number of accidents" << std::endl;
+            std::cout << "Please enter in 0 to quit and show the region with the lowest number of accidents" << std::endl;
+            std::cin >> user_option;
+        }
+        else{
+            getRegInfo(region_name, num_accidents);
+            if (lowest_accidents == 0 && count == 0){
+                lowest_accidents = num_accidents;
+                safest_region = region_name;
+                count += 1;
+                }
+            else if (isLower(num_accidents, lowest_accidents)){
+                safest_region = region_name;
+                lowest_accidents = num_accidents;
+                count += 1;
+            }
+            std::cout << "Please enter in 1 to input the region name and number of accidents" << std::endl;
+            std::cout << "Please enter in 0 to quit and show the region with the lowest number of accidents" << std::endl;
+            std::cin >> user_option;
+        }
+    }
+
+    std::cout << "Safest region: " << safest_region << std::endl;
+    std::cout << "Accidents in the region: " << lowest_accidents << std::endl; 
 
     return 0;
 }
