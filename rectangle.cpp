534d0e2f1eb1f7c779a2ee8955ff878929691566
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-22 23:28:47 -0400

    Created core functionality of rectangle program, successfully tested.

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 
-float getLengthWidth();
-float calcPerimeter();
-float calcArea();
-void displayProperties();
+void getLengthWidth(float &length, float &width);
+float calcPerimeter(float length, float width);
+float calcArea(float length, float width);
+void displayProperties(float perimeter, float area);
+
+float rectangle_length;
+float rectangle_width;
 
 int main(){
-
-
-
+    
+    getLengthWidth(rectangle_length, rectangle_width);
+    displayProperties(rectangle_length, rectangle_width);
     return 0;
 }
 
-float getLengthWidth(){
+void getLengthWidth(float &length, float &width){
 
+    std::cout << "Please enter in the length of the rectangle: ";
+    std::cin >> length;
+    std::cout << "Please enter in the width of the rectangle: ";
+    std::cin >> width;
+
+    while (length < 0 || width < 0){
+    if (length < 0 && width < 0){
+        std::cout << "Invalid length and width measurements, please try again." << std::endl;
+        std::cout << "Please enter in the length of the rectangle: ";
+        std::cin >> length;
+        std::cout << "Please enter in the width of the rectangle: ";
+        std::cin >> width;
+        }
+        else if (length < 0){
+        std::cout << "Invalid length measurement, please try again." << std::endl;
+        std::cout << "Please enter in the length of the rectangle: ";
+        std::cin >> length;
+        }
+        else if (width < 0){
+            std::cout << "Invalid width measurement, please try again." << std::endl;
+            std::cout << "Please enter in the width of the rectangle: ";
+            std::cin >> width;
+        }
+    }
 }
 
-float calcPerimeter(){
+float calcPerimeter(float length, float width){
+    return 2 * length + 2 * width;
+    }
 
+float calcArea(float length, float width){
+    return length * width;
 }
 
-float calcArea(){
+void displayProperties(float length, float width){
 
-}
-
-void displayProperties(){
-    
+    float perimeter = calcPerimeter(length, width);
+    float area = calcArea(length, width);
+    std::cout << "The perimeter of the rectangle is: " << perimeter << std::endl;
+    std::cout << "The area of the rectangle is: " << area << std::endl;
 }
