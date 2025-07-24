b39ce1f6e40588e9d6b3adfb43db3b175fa6c971
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-23 21:15:40 -0400

    Edited program for succinctness and so that output was properly formatted.

--- temperature.cpp
+++ temperature.cpp
@@ -8,16 +8,14 @@ int end_temp = 20;
 int main(){
 
     std::cout << std::setw(10) << "Fahrenheit" << std::setw(15) << "Celsius" << std::endl;
-    std::cout << std::fixed << std::showpoint << std::setprecision(2);
-    for (int i = start_temp; i < end_temp; i++){
-        float celsius = getCelsius(i);
-        std::cout << std::setw(10) << i << std::setw(15) << celsius << std::endl;
+    std::cout << std::fixed << std::showpoint << std::setprecision(1);
+    for (int i = start_temp; i < end_temp + 1; i++){
+        std::cout << std::setw(10) << i << std::setw(15) << getCelsius(i) << std::endl;
     }
 
     return 0;
 }
 
 float getCelsius(int temp){
-    float temp_celsius = (5 * (temp -32)) / 9;
-    return temp_celsius;
+    return (5 * (temp - 32)) / 9.0;
 }
